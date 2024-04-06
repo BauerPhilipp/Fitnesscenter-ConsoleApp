fb58dadb94ade40a0f8468747f9c071b073d7057
BauerPhilipp <fam.bauer@live.at>
2024-04-06 09:17:25 +0200

    Eingabe Geburtstag fertig

--- Tag2 Fitnesscenter/Program.cs
+++ Tag2 Fitnesscenter/Program.cs
@@ -3,12 +3,16 @@
     internal class Program
     {
 
-        private List<Wertkarte> wertkarten = new List<Wertkarte>();
+        private static List<Wertkarte> wertkarten = new List<Wertkarte>();
 
         static void Main(string[] args)
         {
-            StartDialog();
 
+            Console.WriteLine("Hallo");
+            Console.WriteLine("Um ein neues Konto anzulegen drücke N");
+            Console.WriteLine("Für die Verwaltung eines bestehenden Kontos drücke V");
+            EingabeverarbeitungKontoNeuOderVerwalten(Console.ReadLine());
+            
 
 
 
@@ -24,9 +28,67 @@
             //    $"w3 Vorname: {w3.Vorname}");
         }
 
-        private static void StartDialog()
+        public static void EingabeverarbeitungKontoNeuOderVerwalten(string eingabeString)
         {
-            Console.WriteLine();
+            switch (eingabeString.ToUpper())
+            {
+                case "N":
+                    Console.WriteLine("Zur Einrichtung einer neuen Wertkarte benötigen wir vorab einige Angaben");
+                    NeueWertkarteEinrichten();
+                    break;
+                case "V":
+                    break;
+                default:
+                    Console.WriteLine("Ungültige Eingabe!");
+                    Console.WriteLine("Gültige Eingaben sind V und N");
+                    EingabeverarbeitungKontoNeuOderVerwalten(Console.ReadLine());
+                    break;
+            }
         }
+
+        public static void NeueWertkarteEinrichten()
+        {
+            DateOnly geburtsDatum = new DateOnly();
+            int geburtsJahr = 0;
+            int geburtsMonat = 0;
+            int geburtsTag = 0;
+
+            // Geburtsjahr einlesen
+            do
+            {
+                Console.WriteLine("Geburtsjahr z.B. 1984");
+            } while (!(int.TryParse(Console.ReadLine(), out geburtsJahr) && geburtsJahr > 0 && geburtsJahr < 9999));
+
+            // Geburtsmonat einlesen
+            do
+            {
+                Console.WriteLine("Geburtsmonat z.B. 9");
+
+            } while (!(int.TryParse(Console.ReadLine(), out geburtsMonat) && geburtsMonat > 0 && geburtsMonat < 13));
+
+            // für die überprüfung der maximalen Tage in diesem monat
+            geburtsDatum = new DateOnly(geburtsJahr, geburtsMonat, 1);
+
+            // Geburtstag einlesen
+            do
+            {
+                Console.WriteLine("Geburtstag z.B. 28");
+
+
+            } while (!(int.TryParse(Console.ReadLine(), out geburtsTag) && 
+            geburtsTag > 0 &&
+            geburtsTag <= DateTime.DaysInMonth(geburtsDatum.Year, geburtsDatum.Month)));
+
+
+            geburtsDatum = new DateOnly(geburtsJahr, geburtsMonat, geburtsTag);
+            Console.WriteLine($"Du wurdest am {geburtsDatum} geboren");
+            if (geburtsDatum == new DateOnly(geburtsJahr, DateTime.Now.Month, DateTime.Now.Day))
+            {
+                Console.WriteLine("   Du hast heute Geburtstag!");
+                Console.WriteLine("      !!HAPPY BIRTHDAY!!");
+            }
+        }
+        
+
     }
 }
